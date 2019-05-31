a1b97e5a8764dc43a5413752826fefd3992fe9e2
Enrinal Zulhimar <muhamadenrinal@gmail.com>
2019-05-31 14:26:45 +0700

    Input to Day by Day Directory

---
+++ Day Two - Day Three/Simple-Order-System-Management-Golang/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"order/domain"
+	"order/usecase"
+)
+
+func main() {
+	cust := domain.Customer{1, "Enrinal"}
+	user := usecase.User{1, false, 1, 1, 1, cust}
+	sapi := domain.Item{1, "Sapi", true}
+	mobil := domain.Item{2, "Mobil", true}
+	order := usecase.Order{1, user.Customer, []domain.Item{sapi, mobil}, 1}
+	user.AmbilBarang(sapi)
+	usecase.PrintUser(user)
+	order.ProsesOrder()
+	usecase.PrintOrder(order)
+}
---
+++ Day Two - Day Three/Simple-Order-System-Management-Golang/usecase/usecase.go
@@ -0,0 +1,70 @@
+package usecase
+
+import (
+	"fmt"
+	"order/domain"
+)
+
+type User struct {
+	Id             int
+	IsSeller       bool
+	CountPembelian int
+	CountItem      int
+	Status         int
+	Customer       domain.Customer
+}
+type Order domain.Order
+
+func IsPembelian(pembeli User) bool {
+	if pembeli.Status == 1 && pembeli.CountPembelian <= 5 && pembeli.CountItem <= 10 {
+		return true
+	} else if pembeli.Status == 2 && pembeli.CountItem <= 10 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (pembeli *User) BeliBarang() bool {
+	if IsPembelian(*pembeli) == true {
+		pembeli.CountPembelian++
+		return true
+	} else {
+		return false
+	}
+}
+
+func (pembeli *User) AmbilBarang(item domain.Item) bool {
+	if IsPembelian(*pembeli) == true {
+		pembeli.CountItem++
+		//domain.Order{pembeli.Id,pembeli.Customer,[]domain.Item{item}}
+		return true
+	} else {
+		return false
+	}
+}
+
+func PrintUser(pembeli User) {
+	fmt.Printf("%d %s %d %d %d \n", pembeli.Id, pembeli.Customer.Name, pembeli.CountItem, pembeli.CountPembelian, pembeli.Status)
+}
+
+func (order *Order) ProsesOrder() bool {
+	order.Status = 2
+	return true
+}
+
+func (order *Order) DikirimOrder() bool {
+	order.Status = 3
+	return true
+}
+
+func (order *Order) DiterimaOrder() bool {
+	order.Status = 4
+	return true
+}
+
+func PrintOrder(order Order) {
+	fmt.Println("Order id: ", order.Id)
+	fmt.Println("Customer name: ", order.Customer.Name)
+	fmt.Println("Status : ", order.Status)
+}
