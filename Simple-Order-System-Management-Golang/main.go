f6a9c0355ae4fe623a8842b082b2c76a3129d0bf
Enrinal Zulhimar <muhamadenrinal@gmail.com>
2019-06-02 19:45:07 +0700

    Change Function name to English

--- Simple-Order-System-Management-Golang/main.go
+++ Simple-Order-System-Management-Golang/main.go
@@ -11,8 +11,8 @@ func main() {
 	sapi := domain.Item{1, "Sapi", true}
 	mobil := domain.Item{2, "Mobil", true}
 	order := usecase.Order{1, user.Customer, []domain.Item{sapi, mobil}, 1}
-	user.AmbilBarang(sapi)
+	user.AddItem(sapi)
 	usecase.PrintUser(user)
-	order.ProsesOrder()
+	order.OrderProcess()
 	usecase.PrintOrder(order)
 }
