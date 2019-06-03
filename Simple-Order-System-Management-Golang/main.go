102479839738bba1102d12d00c64d81bf7e04572
Enrinal Zulhimar <muhamadenrinal@gmail.com>
2019-06-03 16:28:06 +0700

    Update main.go

--- Simple-Order-System-Management-Golang/main.go
+++ Simple-Order-System-Management-Golang/main.go
@@ -5,16 +5,9 @@ import (
 	"order/usecase"
 )
 
-const REGULARBUYER = 1
-const SUBCRIPTIONBUYER = 2
-const PENDING = 1
-const PROCESS = 2
-const DDELIVERED = 3
-const SEND = 2
-
 func main() {
 	cust := domain.Customer{1, "Enrinal"}
-	user := usecase.User{1, false, 1, 1, REGULARBUYER, cust}
+	user := usecase.User{1, false, 1, 1, usecase.REGULARBUYER, cust}
 	sapi := domain.Item{1, "Sapi", true}
 	mobil := domain.Item{2, "Mobil", true}
 	order := usecase.Order{1, user.Customer, []domain.Item{sapi, mobil}, 1}
