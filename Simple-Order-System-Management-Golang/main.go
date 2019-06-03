ee0a5867a20be020c9527bcefab006e1109fe4ff
Muhamad Enrinal Zulhimar <enrinal@warungpintar.co>
2019-06-03 16:43:35 +0700

    Update main.go

--- Simple-Order-System-Management-Golang/main.go
+++ Simple-Order-System-Management-Golang/main.go
@@ -9,8 +9,9 @@ func main() {
 	cust := domain.Customer{1, "Enrinal"}
 	user := usecase.User{1, false, 1, 1, usecase.REGULARBUYER, cust}
 	sapi := domain.Item{1, "Sapi", true}
+	rumah := domain.Item{3, "Rumah", true}
 	mobil := domain.Item{2, "Mobil", true}
-	order := usecase.Order{1, user.Customer, []domain.Item{sapi, mobil}, 1}
+	order := usecase.Order{1, user.Customer, []domain.Item{sapi, mobil, rumah}, 1}
 	user.AddItem(sapi)
 	usecase.PrintUser(user)
 	order.OrderProcess()
