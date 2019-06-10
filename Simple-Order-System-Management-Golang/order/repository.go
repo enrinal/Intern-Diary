86ef38773cdebebb5bd3b4deb65d50b9baa8d2d9
Muhamad Enrinal Zulhimar <enrinal@warungpintar.co>
2019-06-10 16:56:50 +0700

    Add GetAllOrder and GetOrderById to Order Repository

---
+++ Simple-Order-System-Management-Golang/order/repository.go
@@ -0,0 +1,14 @@
+package order
+
+import (
+	"gitlab.warungpintar.co/enrinal/intern-diary/simple-order/models"
+)
+
+type Repository interface {
+	GetAllOrder() ([]*models.Order, error)
+	GetOrderById(ID int64) (*models.Order, error)
+	ChangeOrderSend(ID int64) (error)
+	ChangeOrderProcess(ID int64) (error)
+	ChangeOrderDelivered(ID int64) (error)
+	GetOrderStatus(ID int64)(status string)
+}
---
+++ Simple-Order-System-Management-Golang/order/repository/order_repository.go
@@ -0,0 +1,16 @@
+package repository
+
+import (
+	"gitlab.warungpintar.co/enrinal/intern-diary/simple-order/models"
+)
+
+
+func GetAllOrder() ([]*models.Order, error) {
+	listorder := make([]*models.Order, 0)
+	return listorder, nil
+}
+
+func GetOrderById(ID int64) (*models.Order, error) {
+	order := &models.Order{}
+	return order, nil
+}
