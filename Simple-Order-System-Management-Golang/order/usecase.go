4571d922b792a26f251e116d6b44c581bd17abd3
Muhamad Enrinal Zulhimar <enrinal@warungpintar.co>
2019-06-20 12:27:43 +0700

    Order Usecase Refactoring

--- Simple-Order-System-Management-Golang/order/usecase.go
+++ Simple-Order-System-Management-Golang/order/usecase.go
@@ -8,9 +8,9 @@ type Usecase interface {
 	GetAllOrder() ([]*models.Order, error)
 	GetAllOrderById(ID int64) ([]*models.Order, error)
 	GetOrderById(ID int64) (*models.Order, error)
-	ChangeOrderSend(ID int64) (error)
-	ChangeOrderProcess(ID int64) (error)
-	ChangeOrderDelivered(ID int64) (error)
-	GetOrderStatus(ID int64)(status string)
-	CountOrderCust(ID int64) (int)
+	ChangeOrderSend(ID int64) error
+	ChangeOrderProcess(ID int64) error
+	ChangeOrderDelivered(ID int64) error
+	CheckLimitOrder(ID int64) bool
+	CountOrderCust(ID int64) int
 }
