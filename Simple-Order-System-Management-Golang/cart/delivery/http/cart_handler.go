f5ede9801a3ad4ec482a97c47bd503e8cfef6aed
Muhamad Enrinal Zulhimar <enrinal@warungpintar.co>
2019-07-11 13:34:00 +0700

    add store cart

--- Simple-Order-System-Management-Golang/cart/delivery/http/cart_handler.go
+++ Simple-Order-System-Management-Golang/cart/delivery/http/cart_handler.go
@@ -9,6 +9,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"gitlab.warungpintar.co/enrinal/intern-diary/simple-order/cart"
 	"gitlab.warungpintar.co/enrinal/intern-diary/simple-order/models"
+	validator "gopkg.in/go-playground/validator.v9"
 )
 
 type ResponseError struct {
@@ -24,6 +25,7 @@ func NewCartHandler(e *echo.Echo, cart cart.Usecase) {
 		CartUsecase: cart,
 	}
 	e.GET("/cart/:id", handler.FetchCustCart)
+	e.POST("/cart", handler.Store)
 }
 
 func (cart *CartHandler) FetchCustCart(c echo.Context) error {
@@ -43,6 +45,29 @@ func (cart *CartHandler) FetchCustCart(c echo.Context) error {
 	return c.JSON(http.StatusOK, listcart)
 }
 
+func (a *CartHandler) Store(c echo.Context) error {
+	var cart models.Cart
+	err := c.Bind(&cart)
+	if err != nil {
+		return c.JSON(http.StatusUnprocessableEntity, err.Error())
+	}
+
+	if ok, err := isRequestValid(&cart); !ok {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	ctx := c.Request().Context()
+	if ctx == nil {
+		ctx = context.Background()
+	}
+
+	err = a.CartUsecase.AddItem(cart)
+
+	if err != nil {
+		return c.JSON(getStatusCode(err), ResponseError{Message: err.Error()})
+	}
+	return c.JSON(http.StatusCreated, cart)
+}
+
 func getStatusCode(err error) int {
 	if err == nil {
 		return http.StatusOK
@@ -58,4 +83,13 @@ func getStatusCode(err error) int {
 	default:
 		return http.StatusInternalServerError
 	}
+}
+
+func isRequestValid(m *models.Cart) (bool, error) {
+	validate := validator.New()
+	err := validate.Struct(m)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
 }
--- Simple-Order-System-Management-Golang/cart/delivery/http/cart_handler_test.go
+++ Simple-Order-System-Management-Golang/cart/delivery/http/cart_handler_test.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 
@@ -11,6 +12,7 @@ import (
 	"github.com/bxcodec/faker"
 	"github.com/labstack/echo"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"gitlab.warungpintar.co/enrinal/intern-diary/simple-order/cart/mocks"
 	"gitlab.warungpintar.co/enrinal/intern-diary/simple-order/models"
@@ -42,5 +44,43 @@ func TestFetchCustCart(t *testing.T) {
 	require.NoError(t, err)
 
 	assert.Equal(t, http.StatusOK, rec.Code)
+	mockUCase.AssertExpectations(t)
+}
+
+func TestStore(t *testing.T) {
+	mockItem := make([]models.Item, 0)
+	mockCart := models.Cart{
+		IDCart: int64(1),
+		IDCust: int64(2),
+		Items:  mockItem,
+		Qty:    int64(1),
+		Prize:  int64(2),
+	}
+
+	tempmockCart := mockCart
+	tempmockCart.IDCart = 1
+	mockUCase := new(mocks.Usecase)
+
+	j, err := json.Marshal(tempmockCart)
+	assert.NoError(t, err)
+
+	mockUCase.On("AddItem", mock.AnythingOfType("models.Cart")).Return(nil)
+
+	e := echo.New()
+	req, err := http.NewRequest(echo.POST, "/cart", strings.NewReader(string(j)))
+	assert.NoError(t, err)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/cart")
+
+	handler := CartHandler{
+		CartUsecase: mockUCase,
+	}
+	err = handler.Store(c)
+	require.NoError(t, err)
+
+	assert.Equal(t, http.StatusCreated, rec.Code)
 	mockUCase.AssertExpectations(t)
 }
