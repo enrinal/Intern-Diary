c1aeccae0c86689e09cd463246f23da6bed4a47f
Muhamad Enrinal Zulhimar <enrinal@warungpintar.co>
2019-07-16 15:56:48 +0700

    make some noice

---
+++ Simple-Order-System-Management-Golang/config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/spf13/viper"
+	"gopkg.in/mgo.v2"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	viper.SetConfigFile(`config.json`)
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		panic(err)
+	}
+	dbHost := viper.GetString(`database.host`)
+	dbPort := viper.GetString(`database.port`)
+	dbUser := viper.GetString(`database.user`)
+	dbName := viper.GetString(`database.dbname`)
+
+	dsn := fmt.Sprintf("%s:@tcp(%s:%s)/%s", dbUser, dbHost, dbPort, dbName)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func ConnectMO() (*mgo.Session, error) {
+	var session, err = mgo.Dial("localhost")
+	if err != nil {
+		return nil, err
+	}
+	return session, nil
+}
---
+++ Simple-Order-System-Management-Golang/config/config_test.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestConnectDB(t *testing.T) {
+	db, err := ConnectDB()
+	if err != nil {
+		t.Error("Error to connect with db")
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		msg := fmt.Sprintf("Ping DB error: %s", err)
+		t.Error(msg)
+	}
+}
+
+func TestConnectMO(t *testing.T) {
+	session, err := ConnectMO()
+	if err != nil {
+		t.Error("Error to Connect with mongo")
+	}
+	defer session.Close()
+}
